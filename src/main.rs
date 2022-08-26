b7d5eb2933085a298a8e14a5d6f76aa84b2aab4b
Andy Li <54951411+SpicyRicecaker@users.noreply.github.com>
2022-08-26 03:34:34 -0700

    switch to regex

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,23 @@
-use percent_encoding::{percent_decode_str, utf8_percent_encode, AsciiSet, CONTROLS};
+use percent_encoding::percent_decode_str;
 use std::io::stdin;
 use std::process::Command;
 
-struct StrStateMachine {
-    idx: usize,
-    start: Option<usize>,
-    end: Option<usize>,
-    first: Vec<char>,
-}
-
-impl StrStateMachine {
-    fn new(state: &str) -> Self {
-        Self {
-            idx: 0,
-            start: None,
-            end: None,
-            first: state.chars().collect(),
-        }
-    }
-
-    fn advance(&mut self, c: char, index: usize) -> bool {
-        if self.first[self.idx] == c {
-            if self.start.is_none() {
-                self.start = Some(index);
-            }
-            self.idx += 1;
-            if self.idx == self.first.len() {
-                self.end = Some(index);
-                false
-            } else {
-                true
-            }
-        } else {
-            self.idx = 0;
-            self.start = None;
-            true
-        }
-    }
-
-    fn is_complete(&self) -> bool {
-        self.end.is_some()
-    }
-}
-
-struct Betweener {
-    first: StrStateMachine,
-    second: StrStateMachine,
-}
-
-impl Betweener {
-    fn new(first: &str, second: &str) -> Self {
-        Self {
-            first: StrStateMachine::new(first),
-            second: StrStateMachine::new(second),
-        }
-    }
-
-    fn between(mut self, string: &str) -> Option<&str> {
-        for (i, c) in string.char_indices() {
-            if !self.first.is_complete() {
-                self.first.advance(c, i);
-            } else if !self.second.is_complete() {
-                self.second.advance(c, i);
-            } else {
-                break;
-            }
-        }
-
-        // could break
-        if let Some(begin) = self.first.end && let Some (end) = self.second.start {
-            Some(&string[begin+1..end])
-        } else {
-            None
-        }
-    }
-}
+use regex::Regex;
 
 enum Method {
-    Search,
     Url,
+    Search,
+}
+
+struct CaptureGetter {
+    regex: Regex,
+    method: Method,
+}
+
+impl CaptureGetter {
+    fn new(regex: Regex, method: Method) -> Self {
+        Self { regex, method }
+    }
 }
 
 fn main() {
@@ -91,56 +30,58 @@ fn main() {
     // single argument
     args.next();
     // long search string we need to replace
-    if let Some(sent_url) = args.next() {
-        let urls = [
-            (
-                "https%3A%2F%2Fwww.bing.com%2Fsearch%3Fq%3D",
-                "%26",
+    if let Some(url_argument) = args.next() {
+        let capture_getters = [
+            CaptureGetter::new(
+                Regex::new("https%3A%2F%2Fwww.bing.com%2Fsearch%3Fq%3D(.*)%26").unwrap(),
                 Method::Search,
             ),
-            (
-                "https%3A%2F%2Fwww.bing.com%2FWS%2Fredirect%2F%3Fq%3D",
-                "%26",
+            CaptureGetter::new(
+                Regex::new("https%3A%2F%2Fwww.bing.com%2FWS%2Fredirect%2F%3Fq%3D(.*)%26").unwrap(),
                 Method::Url,
             ),
             // regex would be useful here for optional matchin
-            ("&url=http%3A%2F%2F", "%2F", Method::Url),
-            ("&url=https%3A%2F%2F", "%2F", Method::Url),
+            CaptureGetter::new(
+                Regex::new("&url=http(?:s)%3A%2F%2F(.*)%2F").unwrap(),
+                Method::Url,
+            ),
         ];
 
-        let s = urls.iter().find_map(|(first, second, method)| {
-            let s = sent_url.as_str().get_between(first, second);
-            s.map(|s| (s, method))
+        let captured = capture_getters.into_iter().find_map(|capture_getter| {
+            capture_getter
+                .regex
+                .captures(&url_argument)
+                .map(|t| (t[1].to_string(), capture_getter.method))
         });
 
-        if let Some((s, method)) = s {
-            let s = percent_decode_str(s)
+        if let Some((captured, method)) = captured {
+            let captured = percent_decode_str(&captured)
                 .decode_utf8()
                 .expect("error unescaping");
             // let s = ;
 
-            println!("{s}");
+            // println!("{s}");
 
             let mut ff = Command::new(r#"C:\Program Files\Firefox Developer Edition\firefox.exe"#);
             match method {
                 Method::Search => {
                     let search_string = "https://google.com/search?q=";
                     ff.arg("--new-tab")
-                        .arg([search_string, &s].join(""))
+                        .arg([search_string, &captured].join(""))
                         .spawn()
                         .expect("ERROR PATH");
                 }
                 Method::Url => {
                     ff.arg("--new-tab")
-                        .arg(s.as_ref())
+                        .arg(captured.as_ref())
                         .spawn()
                         .expect("ERROR PATH");
                 }
             }
-            pause()
+            // pause()
         } else {
-            println!("not implemented `{}`", sent_url);
-            pause()
+            println!("not implemented `{}`", url_argument);
+            // pause()
         }
     } else {
         panic!("search argument not provided");
@@ -159,56 +100,5 @@ fn pause() {
         }
 
         buffer.clear();
-    }
-}
-
-trait Betweenable {
-    fn get_between(&self, first: &str, second: &str) -> Option<&str>;
-}
-
-impl Betweenable for str {
-    fn get_between(&self, first: &str, second: &str) -> Option<&str> {
-        let betweener = Betweener::new(first, second);
-        betweener.between(self)
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    struct FirstMiddleLast {
-        first: String,
-        middle: String,
-        last: String,
-    }
-
-    impl FirstMiddleLast {
-        fn new(first: &str, second: &str, third: &str) -> Self {
-            FirstMiddleLast {
-                first: String::from(first),
-                middle: String::from(second),
-                last: String::from(third),
-            }
-        }
-    }
-
-    #[test]
-    fn testbetween() {
-        let fmls = [
-            FirstMiddleLast::new("fruit", "ILIKEPINEAPPLES", "pineapple"),
-            FirstMiddleLast::new("A", "o", "B"),
-        ];
-
-        for fml in fmls {
-            let full = [&fml.first, &fml.middle, &fml.last]
-                .iter()
-                .map(|&c| String::from(c))
-                .collect::<Vec<String>>()
-                .join("");
-
-            let f = full.get_between(&fml.first, &fml.last);
-            assert_eq!(f, Some(fml.middle.as_str()))
-        }
     }
 }
